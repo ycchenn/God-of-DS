de8a19999b8332c7a0be8381b3bcdf4d4cff96bc
Zgx75 <147365164+Zgx75@users.noreply.github.com>
2024-12-24 17:05:46 +0800

    Update GoogleQueryService.java

--- demo/src/main/java/google/demo/service/GoogleQueryService.java
+++ demo/src/main/java/google/demo/service/GoogleQueryService.java
@@ -88,7 +88,7 @@ public class GoogleQueryService {
         }
 
         // 並行處理結果並計算分數
-        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(50);
         List<Future<SearchResult>> futureResults = new ArrayList<>();
 
         for (Element li : lis) {
